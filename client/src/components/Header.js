640a6148e9addaf053e529bf763e4a04d3cdc144
Tal Franco <tal94franko@gmail.com>
2021-03-16 01:32:44 +0200

    fixed css shit

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -20,49 +20,38 @@ const Header = (props) => {
     history.push('/loginform')
   }
 
-
   return (
     //<nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" >
-      <nav className="Header">
-        <Link to='/' className="navbar-brand"><img className="logo" src="images/Capture.PNG" alt="img"></img>OverCloud</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarResponsive">
+      <div className="Header">
+        {
+            !isLoggedIn ? <div className = "links">
+                <Link to='/Register' className="register">Register</Link>
+             
+                <Link to='/Loginform' className="login">Login</Link>
+              </div>
+              : <ul>
+                  <Link to='/' className="logout" onClick={onLogout}>Logout</Link>
+              </ul>
+          }
+        <Link to='/'><img className="logo" src="images/Capture.PNG" alt="img"></img></Link>
+        <div>
           {
             isLoggedIn ?
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item">
-                  <Link to='/AddCloudAccount' className="nav-link">Add Cloud Account</Link>
+                  <Link to='/AddCloudAccount' className="add-cloud">Add Cloud Account</Link>
                 </li>
                 <li className="nav-item">
-                  <Link to='/CloudAccounts' className="nav-link">Cloud Accounts</Link>
+                  <Link to='/CloudAccounts' className="cloud-acount">Cloud Accounts</Link>
                 </li>
                 <li className="nav-item">
-                  <Link to='/Recommendations' className="nav-link">Recommendations</Link>
+                  <Link to='/Recommendations' className="recommendations">Recommendations</Link>
                 </li>
               </ul> : <div></div>
           }
-          {
-            !isLoggedIn ? <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link to='/Register' className="nav-link">Register</Link>
-              </li>
-              <li className="nav-item">
-                <Link to='/Loginform' className="nav-link">Login</Link>
-              </li></ul>
-              : <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link to='/' className="nav-link" onClick={onLogout}>Logout</Link>
-                </li>
-              </ul>
-          }
+          
         </div>
-      </nav>
-   // </nav >
-    // <div className="Header">
-    //     <img className = "logo" src="images/Capture.PNG"></img>
-    // </div>
+      </div>
   )
 }
 
