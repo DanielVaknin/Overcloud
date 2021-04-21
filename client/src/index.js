6dd5a82c7c46488df245ac467fd35f1a3581249b
Omer Harush <omerharush@gmail.com>
2021-04-21 03:49:34 +0300

    deleted unused import

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'antd/dist/antd.css'
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
