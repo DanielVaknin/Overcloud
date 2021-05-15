c8098f4f5a100799bf08464fb5dac35c2dd72038
Daniel Vaknin <danielvaknin10@gmail.com>
2021-05-15 20:33:44 +0300

    Add mat table

--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -9,6 +9,9 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CloudAccountsComponent } from './components/cloud-accounts/cloud-accounts.component';
 import { RecommendationsComponent } from './components/recommendations/recommendations.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableComponent } from './components/mat-table/mat-table.component';
+import {MatTableModule} from "@angular/material/table";
 
 @NgModule({
   declarations: [
@@ -16,14 +19,17 @@ import { RecommendationsComponent } from './components/recommendations/recommend
     LoginComponent,
     DashboardComponent,
     CloudAccountsComponent,
-    RecommendationsComponent
+    RecommendationsComponent,
+    MatTableComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
