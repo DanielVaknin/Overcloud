ca6dd70f06fed8ecb5787778caabc3f1234aa9fe
Daniel Vaknin <danielvaknin10@gmail.com>
2021-06-03 22:02:09 +0300

    Fix server imports

--- server/api/auth.js
+++ server/api/auth.js
@@ -1,15 +1,10 @@
-const jwt = require('jsonwebtoken');
-const Joi = require('joi');
-const bcrypt = require('bcrypt');
-const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
 const user = require('../controllers/user');
 const passport = require('passport');
-const LocalStrategy = require('passport-local');
 const catchAsync = require('../utils/catchAsync');
 const {schemaUserRegister, schemaUserLogin} = require('../schemas');
-const { validateRequest } = require('../middlewares/SchemaValidator');
+const { validateRequest } = require('../middlewares/schemaValidator');
 
 router.post('/register', validateRequest(schemaUserRegister),catchAsync(user.register))
 router.post('/login', validateRequest(schemaUserLogin), passport.authenticate('local'),(req,res) => {
