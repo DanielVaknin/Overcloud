5a72048051878aae28a1f768f1229461aad96331
Daniel Vaknin <danielvaknin10@gmail.com>
2021-06-07 17:51:45 +0300

    Add prod variables

--- client-angular/src/environments/environment.prod.ts
+++ client-angular/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  // NodeJS URLs
+  authUrl: 'http://overcloud-server:8080/api/auth',
+  cloudAccountsUrl: 'http://overcloud-server:8080/api/cloud-accounts',
+
+  // Python URLs
+  recommendationsUrl: 'http://overcloud-recommendations:5000/recommendations',
+  billingUrl: 'http://overcloud-recommendations:5000/cloud-accounts/billing',
+  cloudAccountsPythonUrl: 'http://overcloud-recommendations:5000/cloud-accounts',
 };
