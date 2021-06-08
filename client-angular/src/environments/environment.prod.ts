62ec62e6808e5dd764d1c07658fc77de8a0d9245
Daniel Vaknin <danielvaknin10@gmail.com>
2021-06-08 14:49:04 +0300

    Change environment urls to point to server fqdn

--- client-angular/src/environments/environment.prod.ts
+++ client-angular/src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
 
   // NodeJS URLs
-  authUrl: 'http://overcloud-server:8080/api/auth',
-  cloudAccountsUrl: 'http://overcloud-server:8080/api/cloud-accounts',
+  authUrl: 'http://overcloud.terasky-labs.com:8080/api/auth',
+  cloudAccountsUrl: 'http://overcloud.terasky-labs.com:8080/api/cloud-accounts',
 
   // Python URLs
-  recommendationsUrl: 'http://overcloud-recommendations:5000/recommendations',
-  billingUrl: 'http://overcloud-recommendations:5000/cloud-accounts/billing',
-  cloudAccountsPythonUrl: 'http://overcloud-recommendations:5000/cloud-accounts',
+  recommendationsUrl: 'http://overcloud.terasky-labs.com:5000/recommendations',
+  billingUrl: 'http://overcloud.terasky-labs.com:5000/cloud-accounts/billing',
+  cloudAccountsPythonUrl: 'http://overcloud.terasky-labs.com:5000/cloud-accounts',
 };
